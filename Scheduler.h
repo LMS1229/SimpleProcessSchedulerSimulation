51ab11aab0f4c9bb91b4f213a26e70fcfefe857e
LMS1229 <32133660+LMS1229@users.noreply.github.com>
2022-04-08 08:16:22 +0900

    Update Scheduler.h

--- Scheduler.h
+++ Scheduler.h
@@ -6,8 +6,8 @@
 #include <stdio.h>
 
 // TODO: 여기서 프로그램에 필요한 추가 헤더를 참조합니다.
-#include "FCFS.h"
-#include "RR.h"
-#include "SJF.h"
-#include "SRT.h"
-#include "MLFQ.h"
+#include "include/FCFS.h"
+#include "include/RR.h"
+#include "include/SJF.h"
+#include "include/SRT.h"
+#include "include/MLFQ.h"
