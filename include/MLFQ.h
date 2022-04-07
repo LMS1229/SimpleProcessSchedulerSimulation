18dc8c8e729d089e980e4795520bfae982c64688
LMS1229 <32133660+LMS1229@users.noreply.github.com>
2022-04-08 08:18:27 +0900

    Update MLFQ.h

--- include/MLFQ.h
+++ include/MLFQ.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "ProcessQueue.h"
 #include "Vector.h"
+#DEFAULT_TIMEQUNTUM 2
 
-vector* MLFQ(process* process_arr, int n, int max_level);
+vector* MLFQ(process* process_arr, int n, int max_level, int(*timequntum)(int, int));
 
-process* Schedueling(processqueue* lq, process* previous_process, int* excute_time, int max_level);		//Get next excute process
+process* Schedueling(processqueue* lq, process* previous_process, int* excute_time, int max_level,int(*timequntum)(int, int));		//Get next excute process
 process* Get_HighestPriorityProcess(processqueue* lq, int max_level);
