0ee1e5cc1ca492891cbbd0d0b91713af3ba8a2df
LMS1229 <32133660+LMS1229@users.noreply.github.com>
2022-04-08 08:21:28 +0900

    Update MLFQ.c

--- include/MLFQ.c
+++ include/MLFQ.c
@@ -1,8 +1,7 @@
 #include "MLFQ.h"
 #include <stdio.h>
 
-vector* MLFQ(process* process_arr, int n, int max_level)
-{
+vector* MLFQ(process* process_arr, int n, int max_level, int(*timequntum)(int, int)){
 	int time = 0, index = 0, excute_time = 0, pid = -1;
 	int all_q_empty = 1;
 	processqueue* lq;		//MLQ
@@ -10,25 +9,20 @@ vector* MLFQ(process* process_arr, int n, int max_level)
 	process* excute = 0;	//prevent excute process 
 	processes_excute_flag = (vector*)malloc(sizeof(vector) * n);
 	lq = (processqueue*)malloc(sizeof(processqueue) * max_level);
-	for (int i = 0; i < n; ++i)
-	{
+	for (int i = 0; i < n; ++i)	{
 		InitVector(processes_excute_flag + i);
 	}
-	for (int i = 0; i < max_level; ++i)
-	{
+	for (int i = 0; i < max_level; ++i)	{
 		InitProcessQueue(&lq[i]);
 	}
-	while (index < n || all_q_empty == 0)
-	{
-		while (index < n && process_arr[index].arrive_time == time)
-		{
+	while (index < n || all_q_empty == 0)	{
+		while (index < n && process_arr[index].arrive_time == time)		{
 			Push_Queue(&lq[0], &process_arr[index]);
 			++index;
 		}
 		//gGt next excute process 
-		excute = Schedueling(lq, excute, &excute_time, max_level);
-		if (excute != 0)
-		{
+		excute = Schedueling(lq, excute, &excute_time, max_level, timequntum);
+		if (excute != 0)		{
 			pid = Excute_Process(excute);
 			excute_time++;
 		}
@@ -59,7 +53,7 @@ vector* MLFQ(process* process_arr, int n, int max_level)
 	return processes_excute_flag;
 }
 
-process* Schedueling(processqueue* lq, process* previous_process, int* excute_time, int max_level)
+process* Schedueling(processqueue* lq, process* previous_process, int* excute_time, int max_level,int(*timequntum)(int, int))
 {
 	process* next_process = 0;
 	if (previous_process == 0)
@@ -71,7 +65,7 @@ process* Schedueling(processqueue* lq, process* previous_process, int* excute_ti
 	}
 	else
 	{
-		if ((1 << previous_process->priority) == *excute_time)
+		if ( *excute_time==(timequntum==0?DEFAULT_TIMEQUNTUM:timequntum(2, previous_process->priority)))
 		{
 			//if all use timequantum
 			//check all queue is empty
